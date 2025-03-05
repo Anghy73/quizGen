ff8265e717595c0f5a97ac10fc604dc4fa39bbbb
Andy <anghy.ruz@gmail.com>
2025-03-05 20:11:27 -0300

    Backgroud Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-function App() {
+import { Lumiflex } from "uvcanvas"
+
+function App () {
+
   return (
     <>
-      <div className="text-7xl text-purple-700 flex justify-center items-center w-screen min-h-screen bg-cyan-950">
-        <p>hi!</p>
+      <div className="w-screen h-screen relative">
+        <Lumiflex />
+        <div className="absolute top-0 left-0 z-20 w-screen h-screen bg-[#0004] backdrop-blur-xl flex justify-center items-center">
+          <h1 className="font-lexend text-white font-bold text-5xl">QuizGen</h1>
+        </div>
       </div>
     </>
   )
